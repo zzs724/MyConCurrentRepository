449400ab3280b3fa369f68f27b4280a345cea546
zzs <65422049@qq.com>
2019-04-21 21:14:29 +0800

    queueProd

--- my-concurrent/src/main/java/com/zzs/queueProd/BlockingQueueTest.java
+++ my-concurrent/src/main/java/com/zzs/queueProd/BlockingQueueTest.java
@@ -5,17 +5,21 @@ import java.util.concurrent.*;
 public class BlockingQueueTest {
     public static void main(String[] args) throws InterruptedException {
         BlockingQueue<String> queue = new LinkedBlockingDeque<>(10);
+//        BlockingQueue<String> queue = new SynchronousQueue<>();
 
         Producer producer = new Producer(queue);
+        Producer producer1 = new Producer(queue);
         Consumer consumer = new Consumer(queue);
 
         ExecutorService executorService = Executors.newCachedThreadPool();
         executorService.execute(producer);
+        executorService.execute(producer1);
         executorService.execute(consumer);
 
         TimeUnit.SECONDS.sleep(5);
 
         producer.stop();
+        producer1.stop();
 
         executorService.shutdown();
     }
