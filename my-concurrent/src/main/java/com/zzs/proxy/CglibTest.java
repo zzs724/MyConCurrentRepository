fb6bfa2c98143277f4c49f9c11a4b2661f4d4c55
EDZ <65422049@qq.com>
2019-04-24 17:24:59 +0800

    udpate

--- my-concurrent/src/main/java/com/zzs/proxy/CglibTest.java
+++ my-concurrent/src/main/java/com/zzs/proxy/CglibTest.java
@@ -1,6 +1,10 @@
 package com.zzs.proxy;
 
 import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
 
 public class CglibTest {
     public static void main(String[] args) {
@@ -10,5 +14,16 @@ public class CglibTest {
         HelloImpl hello = (HelloImpl) enhancer.create();
         hello.sayHello("Hello");
         hello.sayHi("Hi");
+        HelloImpl cgligbProxy = (HelloImpl) new Enhancer().create(new HelloImpl().getClass(), new MethodInterceptor() {
+            @Override
+            public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                System.out.println("匿名内部类");
+                method.invoke( new HelloImpl(),objects);
+                System.out.println("匿名内部类");
+                return null;
+            }
+        });
+        cgligbProxy.sayHello("2222");
     }
+
 }
